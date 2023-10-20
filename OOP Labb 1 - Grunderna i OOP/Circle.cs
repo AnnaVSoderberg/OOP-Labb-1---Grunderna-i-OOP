80778835718f88db54ffcc487c07986f9c4386bb
Anna Söderberg <soderberganna@outlook.com>
2023-10-20 09:09:27 +0200

    Add project files.

---
+++ OOP Labb 1 - Grunderna i OOP/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Labb_1___Grunderna_i_OOP
+{
+    internal class Circle
+    {
+        const float _pi = 3.141f;
+        int _Radius;
+
+        public Circle(int Radius)
+        {
+            _Radius = Radius;
+        }
+
+        public float getArea() 
+        {
+           float area =  _Radius * _Radius *_pi;
+
+            return area;
+        }
+
+        public float getCircumference() 
+        {
+            float circumference = _Radius * 2 * _pi;
+            return circumference;
+        }
+
+        public float getVolume() 
+        {
+            float volume = (_Radius * _Radius * _Radius * _pi * 4)/3;
+            return volume;
+        }
+
+
+
+    }
+}
---
+++ OOP Labb 1 - Grunderna i OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿//Anna Söderberg SUT23
+
+namespace OOP_Labb_1___Grunderna_i_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle area1 = new(5);
+            Circle area2 = new(6);
+
+            Console.WriteLine($"Arean för cirkel 1 är {area1.getArea()}.");
+            Console.WriteLine($"Omkretsen för cirkel 1 är {area1.getCircumference()}.");
+            Console.WriteLine($"volymen för cirkel 1 är {area1.getVolume()}.");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Arean för cirkel 2 är {area2.getArea()}.");
+            Console.WriteLine($"Omkretsen för cirkel 2 är {area2.getCircumference()}.");
+            Console.WriteLine($"volymen för cirkel 2 är {area2.getVolume()}.");
+
+        }
+    }
+}
